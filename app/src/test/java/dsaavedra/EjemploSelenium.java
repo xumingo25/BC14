348adb1345560ed232de591dead53a5aaf8e8270
Pobre Domingo <domingo.saavedra@tsoftglobal.com>
2023-09-21 11:23:07 -0300

    Se actualiza repositorio. Se sube ejemplo de uso de webdriver

---
+++ app/src/test/java/dsaavedra/EjemploSelenium.java
@@ -0,0 +1,18 @@
+package dsaavedra;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EjemploSelenium {
+    public static void main(String[] args) {
+        String rutaDriver = "C:\\Users\\domingo.saavedra\\Desktop\\BC14\\app\\src\\test\\resources\\drivers\\chromedriver.exe";
+
+        //Enlazar el driver (manejador)
+        System.setProperty("webdriver.chrome.driver",rutaDriver);
+
+        //Instanciar un objeto de tipo WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+    }
+}
